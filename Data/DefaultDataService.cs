511a665e4c4556f5758b122532d54539d7a8e582
jallen <jallen@snow.edu>
2018-10-25 09:55:42 -0600

    Add button to browse for file

--- Data/DefaultDataService.cs
+++ Data/DefaultDataService.cs
@@ -1,9 +1,11 @@
-﻿using GeneGenie.Gedcom.Parser;
+﻿using Avalonia.Controls;
+using GeneGenie.Gedcom.Parser;
 using Interfaces;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Data
 {
@@ -29,6 +31,19 @@ namespace Data
         public bool FileExists(string gedcomFile)
         {
             return File.Exists(gedcomFile);
+        }
+
+        public async Task<string> FindFile()
+        {
+            var openFileDialog = new OpenFileDialog()
+            {
+                AllowMultiple=false,
+                Title="What Gedcom file do you want to use?"
+            };
+            var pathArray = await openFileDialog.ShowAsync();
+            if (pathArray.Length > 0)
+                return pathArray[0];
+            return null;
         }
     }
 }
